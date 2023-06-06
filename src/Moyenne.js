71f405b2ecf176d1e06b5847220d017d97ad25af
MOHAMMED TAHER <100471051+simohammed-taher@users.noreply.github.com>
2023-06-06 19:28:09 +0000

    06/06/2023

--- src/Moyenne.js
+++ src/Moyenne.js
@@ -2,33 +2,15 @@ import "./index.css"
 import React, { useState } from 'react';
 function Moyenne() {
 	const [nom, setNom] = useState('')
-	const [phy, setPhy] = useState(0)
-	const [math, setMath] = useState(0)
-	const [franc, setFranc] = useState(0)
-	const [moye, setMoye] = useState(0)
-	function handleChange(e) {
-		console.log(e.target.id);
-		switch (e.target.id) {
-			case 'nom':
-				setNom(() => e.target.value);
-				break;
-			case 'phy':
-            setPhy(() => e.target.value);
-				break;
-			case 'math':
-            setMath(() => e.target.value);
-				break;
-			case 'franc':
-            setFranc(() => e.target.value);
-				break;
-                default:
-                break;
-		}
-	}
+	const [phy, setPhy] = useState()
+	const [math, setMath] = useState()
+	const [franc, setFranc] = useState()
+	const [moye, setMoye] = useState()
+	
 	function calculer() {
-        let m=(parseFloat(phy)+parseFloat(math)+parseFloat(franc))/3 ;
-        setMoye(()=>m.toFixed(2))
-    }
+		let m = (parseFloat(phy) + parseFloat(math) + parseFloat(franc)) / 3;
+		setMoye(() => m.toFixed(2))
+	}
 	return (
 		<div className='container'>
 			<div>
@@ -36,27 +18,27 @@ function Moyenne() {
 			</div>
 			<div>
 				<label>Nom</label>
-				<input type='text' id='nom' onChange={handleChange} />
+				<input type='text' id='nom' value={nom} onChange={(e)=>setNom(e.target.value)} />
 			</div>
 			<div>
 				<label>Physique</label>
-				<input type='text' id='phy' onChange={handleChange} />
+				<input type='text' id='phy' value={phy} onChange={(e)=>setPhy(e.target.value)}/>
 			</div>
 			<div>
 				<label>Math</label>
-				<input type='text' id='math' onChange={handleChange} />
+				<input type='text' id='math' value={math} onChange={(e)=>setMath(e.target.value)}/>
 			</div>
 			<div>
 				<label>France</label>
-				<input type='text' id='franc' onChange={handleChange} />
+				<input type='text' id='franc' value={franc} onChange={(e)=>setFranc(e.target.value)}/>
 			</div>
-			<button onClick={()=>calculer()}>calculer</button>
-            <p>{nom} vou aver eu en moyen {moye}</p>
-            {moye<=9 && <span style={{ color:"red" }}>FAIBLE</span>}
-            {(moye>9 && moye<=11) && <span style={{ color:"yellow" }}>PASSABLE</span>}
-            {(moye>11 && moye<13) && <span style={{ color:"green" }}>BEIN</span>}
-            {(moye>13 && moye<=16) && <span style={{ color:"blue" }}>TRES BEIN</span>}
-            {moye>16 && <span style={{ color:"orange" }}>EXCELLENT</span>}
+			<button onClick={() => calculer()}>calculer</button>
+			<p>{nom} vou aver eu en moyen {moye}</p>
+			{moye <= 9 && <span style={{ color: "red" }}>FAIBLE</span>}
+			{(moye > 9 && moye <= 11) && <span style={{ color: "yellow" }}>PASSABLE</span>}
+			{(moye > 11 && moye < 13) && <span style={{ color: "green" }}>BEIN</span>}
+			{(moye > 13 && moye <= 16) && <span style={{ color: "blue" }}>TRES BEIN</span>}
+			{moye > 16 && <span style={{ color: "orange" }}>EXCELLENT</span>}
 		</div>
 	);
 }
